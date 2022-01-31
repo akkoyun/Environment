2ec80854bd0e0e770d1d9398a410e896ad02335f
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-01-31 16:54:20 +0300

    03.02.02 - Initial update

--- examples/MPL3115A2_Pressure/MPL3115A2_Pressure.ino
+++ examples/MPL3115A2_Pressure/MPL3115A2_Pressure.ino
@@ -1,40 +1,36 @@
 #include "Environment.h"
 
 void setup() {
-
-	// Serial Communication Start
-	Serial.begin(115200);
-
-	// Start I2C
-	Wire.begin();
-
-	// Header
-	Serial.println("    MPL3115A2 Pressure    ");
-	Serial.println("--------------------------");
+  
+  // Serial Communication Start
+  Serial.begin(115200);
+  
+  // Start I2C
+  Wire.begin();
+  
+  // Header
+  Serial.println("    MPL3115A2 Pressure    ");
+  Serial.println("--------------------------");
 
 }
 
 void loop() {
+  
+  // Set Start Time
+  unsigned long Time = millis();
 
-	// Set Start Time
-	uint32_t Time = millis();
+  // Measure
+  float _Measurement = Sensor.MPL3115A2_Pressure();
 
-	// Measure
-	float _Measurement = Sensor.MPL3115A2_Pressure();
-
-	// Calculate Delta Time
-	uint32_t DT = millis() - Time;
-
-	// Serial Print Data
-	Serial.print("Value         : ");
-	Serial.print(_Measurement, 3);
-	Serial.println(" mBar");
-	Serial.print("Function Time : ");
-	Serial.print(DT);
-	Serial.println(" mS");
-	Serial.println("--------------------------");
-
-	// Loop Delay
-	delay(1000);
+  // Calculate Delta Time
+  long DT = millis() - Time;
+    
+  // Serial Print Data
+  Serial.print("Value         : "); Serial.print(_Measurement, 3); Serial.println(" mBar");
+  Serial.print("Function Time : "); Serial.print(DT); Serial.println(" mS");
+  Serial.println("--------------------------");
+  
+  // Loop Delay
+  delay(1000);
 
 }
--- examples/TSL2561_Light/TSL2561_Light.ino
+++ examples/TSL2561_Light/TSL2561_Light.ino
@@ -1,40 +1,36 @@
 #include "Environment.h"
 
 void setup() {
-
-	// Serial Communication Start
-	Serial.begin(115200);
-
-	// Start I2C
-	Wire.begin();
-
-	// Header
-	Serial.println("      TSL2561 Light       ");
-	Serial.println("--------------------------");
+  
+  // Serial Communication Start
+  Serial.begin(115200);
+  
+  // Start I2C
+  Wire.begin();
+  
+  // Header
+  Serial.println("      TSL2561 Light       ");
+  Serial.println("--------------------------");
 
 }
 
 void loop() {
+  
+  // Set Start Time
+  unsigned long Time = millis();
 
-	// Set Start Time
-	uint32_t Time = millis();
+  // Measure
+  float _Measurement = Sensor.TSL2561_Light();
 
-	// Measure
-	float _Measurement = Sensor.TSL2561_Light();
-
-	// Calculate Delta Time
-	uint32_t DT = millis() - Time;
-
-	// Serial Print Data
-	Serial.print("Value         : ");
-	Serial.print(_Measurement, 3);
-	Serial.println(" Lux");
-	Serial.print("Function Time : ");
-	Serial.print(DT);
-	Serial.println(" mS");
-	Serial.println("--------------------------");
-
-	// Loop Delay
-	delay(1000);
+  // Calculate Delta Time
+  long DT = millis() - Time;
+    
+  // Serial Print Data
+  Serial.print("Value         : "); Serial.print(_Measurement, 3); Serial.println(" Lux");
+  Serial.print("Function Time : "); Serial.print(DT); Serial.println(" mS");
+  Serial.println("--------------------------");
+  
+  // Loop Delay
+  delay(1000);
 
 }
