dd0c85caf2f218b01b9ef7482729fa6723e95e48
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-02-20 13:40:36 +0300

    Bug fix

--- src/Environment.h
+++ src/Environment.h
@@ -2,7 +2,7 @@
 #define __Environment__
 
 	// Include Arduino Library
-	#ifndef __Arduino__
+	#ifndef Arduino_h
 		#include <Arduino.h>
 	#endif
 
@@ -563,7 +563,7 @@
 				I2C_Functions::Begin();
 
 				// Control for Device
-				if (I2C_Functions::Variables.Device.Detect) {
+				if (I2C_Functions::Detect()) {
 
 					// Reset Sensor
 					this->Reset();
@@ -962,32 +962,6 @@
 				
 			}
 
-			/* I2C Functions */
-
-			// Read address Function
-			uint8_t Address(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Device.Address);
-
-			}
-
-			// Read detect Function
-			bool Detect(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Device.Detect);
-
-			}
-
-			// Read Mux Channel Function
-			uint8_t Mux_Channel(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Multiplexer.Channel);
-
-			}
-
 	};
 
 	// MPL3115A2 Pressure Sensor Class
@@ -1144,32 +1118,6 @@
 					} 
 
 			};
-
-			/* I2C Functions */
-
-			// Read address Function
-			uint8_t Address(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Device.Address);
-
-			}
-
-			// Read detect Function
-			bool Detect(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Device.Detect);
-
-			}
-
-			// Read Mux Channel Function
-			uint8_t Mux_Channel(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Multiplexer.Channel);
-
-			}
 
 	};
 
@@ -1746,32 +1694,6 @@
 
 			}
 
-			/* I2C Functions */
-
-			// Read address Function
-			uint8_t Address(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Device.Address);
-
-			}
-
-			// Read detect Function
-			bool Detect(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Device.Detect);
-
-			}
-
-			// Read Mux Channel Function
-			uint8_t Mux_Channel(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Multiplexer.Channel);
-
-			}
-
 	};
 
 	// Si1145-A19 Light Sensor Class
@@ -1929,32 +1851,6 @@
 
 			}
 
-			/* I2C Functions */
-
-			// Read address Function
-			uint8_t Address(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Device.Address);
-
-			}
-
-			// Read detect Function
-			bool Detect(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Device.Detect);
-
-			}
-
-			// Read Mux Channel Function
-			uint8_t Mux_Channel(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Multiplexer.Channel);
-
-			}
-
 	};
 
 	// NA2302 Pressure Sensor Class
@@ -1997,7 +1893,7 @@
 				I2C_Functions::Begin();
 
 				// Control for Device
-				if (I2C_Functions::Variables.Device.Detect) {
+				if (I2C_Functions::Detect()) {
 				
 					// End Function
 					return(true);
@@ -2162,265 +2058,6 @@
 					return(_Temperature_Measurement_Array[0]);
 
 				} 
-
-			}
-
-			/* I2C Functions */
-
-			// Read address Function
-			uint8_t Address(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Device.Address);
-
-			}
-
-			// Read detect Function
-			bool Detect(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Device.Detect);
-
-			}
-
-			// Read Mux Channel Function
-			uint8_t Mux_Channel(void) {
-
-				// Return Address
-				return(I2C_Functions::Variables.Multiplexer.Channel);
-
-			}
-
-	};
-
-	// Weather Sensor Class
-	class B108AA_Environment : private HDC2010, private MPL3115A2, private SI1145 {
-
-		// Public Context		
-		public:
-
-			// Construct a new B108AA_Environment object
-			B108AA_Environment(void) : HDC2010(true, __B108AA_MUX_HDC2010__), MPL3115A2(true, __B108AA_MUX_MPL3115__), SI1145(true, __B108AA_MUX_SI1145__) {
-
-			}
-
-			// Begin Sensor
-			void Start(void) {
-
-				// Start HDC2010
-				HDC2010::Begin();
-
-				// Calibrate HDC2010
-				HDC2010::Set_Calibration_Parameters(1, 1.0053, -0.4102);
-				HDC2010::Set_Calibration_Parameters(2, 0.9821, -0.3217);
-
-				// Start MPL3115A2
-				MPL3115A2::Begin();
-
-				// Calibrate MPL3115A2
-				MPL3115A2::Set_Calibration_Parameters(1, 0);
-
-				// Start SI1145
-				SI1145::Begin();
-
-			}
-
-			// Read Temperature Function
-			float Temperature(void) {
-
-				// Return Temperature
-				return(HDC2010::Temperature(10));
-
-			}
-
-			// Read Humidity Function
-			float Humidity(void) {
-
-				// Return Humidity
-				return(HDC2010::Humidity(10));
-
-			}
-
-			// Read Pressure Function
-			float Pressure(void) {
-
-				// Return Pressure
-				return(MPL3115A2::Pressure(1));
-
-			}
-
-			// Read Visible Light Function
-			float Visible(void) {
-
-				// Return Visible Light
-				return(SI1145::Read_Visible());
-
-			}
-
-			// Read IR Light Function
-			float IR(void) {
-
-				// Return IR Light
-				return(SI1145::Read_IR());
-
-			}
-
-			// Read UV Function
-			float UV(void) {
-
-				// Return UV
-				return(SI1145::Read_UV());
-
-			}
-
-			// Read Delta Pressure Function
-			void Wind(float & _WD, float & _WS) {
-
-				// Define Sensor Object
-				SDP810 Sensor_Axis_1(true, __B108AA_MUX_SDP810_X__);
-				SDP810 Sensor_Axis_2(true, __B108AA_MUX_SDP810_Y__);
-				SDP810 Sensor_Axis_3(true, __B108AA_MUX_SDP810_Z__);
-
-				// Begin Sensor
-				Sensor_Axis_1.Begin();
-				Sensor_Axis_2.Begin();
-				Sensor_Axis_3.Begin();
-
-				// Declare Variables
-				struct Wind_Speed_Struct {
-
-					// Axis 1 Struct
-					struct Wind_Speed_A_Struct {
-						float Speed;
-						float X;
-						float Y;
-					} Axis_1;
-					
-					// Axis 2 Struct
-					struct Wind_Speed_B_Struct {
-						float Speed;
-						float X;
-						float Y;
-					} Axis_2;
-
-					// Axis 3 Struct
-					struct Wind_Speed_C_Struct {
-						float Speed;
-						float X;
-						float Y;
-					} Axis_3;
-
-					// X Vector Sum
-					float X_Vectors_SUM;
-
-					// Y Vector Sum
-					float Y_Vector_SUM;
-
-				} _Wind_Speed;
-
-				// Declare Constants
-				const double _Rho = 1.225;
-
-				// Calculate Wind Speed
-				_Wind_Speed.Axis_1.Speed = sqrt(2 * Sensor_Axis_1.Pressure(50) / _Rho);
-				_Wind_Speed.Axis_2.Speed = sqrt(2 * Sensor_Axis_2.Pressure(50) / _Rho);
-				_Wind_Speed.Axis_3.Speed = sqrt(2 * Sensor_Axis_3.Pressure(50) / _Rho);
-
-				// Calculate Wind Direction Axis 1
-				if (_Wind_Speed.Axis_1.Speed > 0) {
-
-					// Calculate Wind Direction X Vector
-					_Wind_Speed.Axis_1.X = _Wind_Speed.Axis_1.Speed * cos(radians(0));
-
-					// Calculate Wind Direction Y Vector
-					_Wind_Speed.Axis_1.Y = _Wind_Speed.Axis_1.Speed * sin(radians(0));
-
-				} else if (_Wind_Speed.Axis_1.Speed < 0) {
-
-					// Calculate Wind Direction X Vector
-					_Wind_Speed.Axis_1.X = abs(_Wind_Speed.Axis_1.Speed) * cos(radians(180));
-
-					// Calculate Wind Direction Y Vector
-					_Wind_Speed.Axis_1.Y = abs(_Wind_Speed.Axis_1.Speed) * sin(radians(180));
-
-				} else if (_Wind_Speed.Axis_1.Speed == 0) {
-
-					// Calculate Wind Direction X Vector
-					_Wind_Speed.Axis_1.X = 0;
-
-					// Calculate Wind Direction Y Vector
-					_Wind_Speed.Axis_1.Y = 0;
-
-				}
-
-				// Calculate Wind Direction Axis 2
-				if (_Wind_Speed.Axis_2.Speed > 0) {
-
-					// Calculate Wind Direction X Vector
-					_Wind_Speed.Axis_2.X = _Wind_Speed.Axis_2.Speed * cos(radians(60));
-
-					// Calculate Wind Direction Y Vector
-					_Wind_Speed.Axis_2.Y = _Wind_Speed.Axis_2.Speed * sin(radians(60));
-
-				} else if (_Wind_Speed.Axis_2.Speed < 0) {
-
-					// Calculate Wind Direction X Vector
-					_Wind_Speed.Axis_2.X = abs(_Wind_Speed.Axis_2.Speed) * cos(radians(240));
-
-					// Calculate Wind Direction Y Vector
-					_Wind_Speed.Axis_2.Y = abs(_Wind_Speed.Axis_2.Speed) * sin(radians(240));
-
-				} else if (_Wind_Speed.Axis_2.Speed == 0) {
-
-					// Calculate Wind Direction X Vector
-					_Wind_Speed.Axis_2.X = 0;
-
-					// Calculate Wind Direction Y Vector
-					_Wind_Speed.Axis_2.Y = 0;
-
-				}
-
-				// Calculate Wind Direction Axis 3
-				if (_Wind_Speed.Axis_3.Speed > 0) {
-
-					// Calculate Wind Direction X Vector
-					_Wind_Speed.Axis_3.X = _Wind_Speed.Axis_3.Speed * cos(radians(120));
-
-					// Calculate Wind Direction Y Vector
-					_Wind_Speed.Axis_3.Y = _Wind_Speed.Axis_3.Speed * sin(radians(120));
-
-				} else if (_Wind_Speed.Axis_3.Speed < 0) {
-
-					// Calculate Wind Direction X Vector
-					_Wind_Speed.Axis_3.X = abs(_Wind_Speed.Axis_3.Speed) * cos(radians(300));
-
-					// Calculate Wind Direction Y Vector
-					_Wind_Speed.Axis_3.Y = abs(_Wind_Speed.Axis_3.Speed) * sin(radians(300));
-
-				} else if (_Wind_Speed.Axis_3.Speed == 0) {
-
-					// Calculate Wind Direction X Vector
-					_Wind_Speed.Axis_3.X = 0;
-
-					// Calculate Wind Direction Y Vector
-					_Wind_Speed.Axis_3.Y = 0;
-
-				}
-
-				// Calculate X Vectors Sum
-				_Wind_Speed.X_Vectors_SUM = _Wind_Speed.Axis_1.X + _Wind_Speed.Axis_2.X + _Wind_Speed.Axis_3.X;
-
-				// Calculate Y Vectors Sum
-				_Wind_Speed.Y_Vector_SUM = _Wind_Speed.Axis_1.Y + _Wind_Speed.Axis_2.Y + _Wind_Speed.Axis_3.Y;
-
-				// Calculate Wind Total Speed
-				_WS = sqrt(pow(_Wind_Speed.X_Vectors_SUM, 2) + pow(_Wind_Speed.Y_Vector_SUM, 2));
-
-				// Calculate Wind Direction
-				_WD = degrees(atan2(_Wind_Speed.Y_Vector_SUM, _Wind_Speed.X_Vectors_SUM));
-
-				// Control for Wind Direction
-				if (_WD < 0) _WD += 360;
 
 			}
 
